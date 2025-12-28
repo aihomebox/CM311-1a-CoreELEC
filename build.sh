85a6e20aba31890263e822afa4d7cc4d709a2309
aihomebox <chatgptnew@sina.com>
2025-12-28 23:08:48 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -158,7 +158,7 @@ if [ -f ${system_root}/usr/share/kodi/.kodi.zip ]; then
 fi
 
 echo "Downloading.kodi.zip file"
-wget -O.kodi.zip "https://ykj-eos-dg5-01.eos-dongguan-6.cmecloud.cn/fb9cf5bd178a473e98a67ba30eb45d43086?response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27.kodi.zip&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20251228T111546Z&X-Amz-SignedHeaders=host&X-Amz-Expires=900&X-Amz-Credential=9T1UKBIX6OJSR5XN2F2T%2F20251228%2Fdefault%2Fs3%2Faws4_request&t=2&u=1039889218191379647&ot=personal&oi=1039889218191379647&f=Fv_CggDPEUuBgMRznSeBB4KwoAOv9REHg&ext=eyJ1dCI6MX0%3D&X-Amz-Signature=5c7337958375a60bffafe98c0fae33537121b756edb3dc6db26de193b3367138"
+wget -O.kodi.zip "https://ykj-eos-dg5-01.eos-dongguan-6.cmecloud.cn/fb9cf5bd178a473e98a67ba30eb45d43086?response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27.kodi.zip&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20251228T150814Z&X-Amz-SignedHeaders=host&X-Amz-Expires=900&X-Amz-Credential=9T1UKBIX6OJSR5XN2F2T%2F20251228%2Fdefault%2Fs3%2Faws4_request&t=2&u=1039889218191379647&ot=personal&oi=1039889218191379647&f=Fv_CggDPEUuBgMRznSeBB4KwoAOv9REHg&ext=eyJ1dCI6MX0%3D&X-Amz-Signature=f41c1b84881ca1abef63c83873aef9f06e23f82011086963a0dd7ea8f3b31331"
 if [ $? -ne 0 ]; then
     echo "下载.kodi.zip 文件失败"
     exit 1
